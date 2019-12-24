be1e76fcd61ec1497912ad5d17fda08ca42bc0c8
andie5 <andieasa@googlemail.com>
2019-12-24 00:37:15 +0000

    children props practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+// function ErrorBox({ children }) {
+//   return <button>{children}</button>;
+// }
+
+function FirstChildOnly({ children }) {
+  const items = React.Children.toArray(children);
+
+  console.log("items", items);
+
+  return <div>First child: {items.slice(0, 1)}</div>;
+}
+
+function LastChildOnly({ children }) {
+  const items = React.Children.toArray(children);
+
+  console.log("items", items);
+
+  return <div>Last child: {items.slice(-1)[0]}</div>;
+}
+
+function Head({ children, number }) {
+  const items = React.Children.toArray(children);
+
+  console.log("items", items);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      1st {number} children: {items.slice(0, number)}
     </div>
   );
 }
+
+function Tail({ children, number }) {
+  const items = React.Children.toArray(children);
+
+  console.log("items", items);
+
+  return (
+    <div>
+      Last {number} children: {items.slice(-number)}
+    </div>
+  );
+}
+
+const App = () => {
+  // return <ErrorBox>This is an error box</ErrorBox>;
+  return (
+    <div>
+      <FirstChildOnly>
+        <div>1st element</div>
+        <div>2nd element</div>
+      </FirstChildOnly>
+      <br />
+      <LastChildOnly>
+        <div>1st element</div>
+        <div>2nd element</div>
+        <div>3rd element</div>
+      </LastChildOnly>
+      <br />
+      <Head number={3}>
+        <div>1st element</div>
+        <div>2nd element</div>
+        <div>3rd element</div>
+        <div>4th element</div>
+        <div>5th element</div>
+      </Head>
+      <br />
+      <Tail number={2}>
+        <div>1st element</div>
+        <div>2nd element</div>
+        <div>3rd element</div>
+        <div>4th element</div>
+      </Tail>
+    </div>
+  );
+};
 
 export default App;
